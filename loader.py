5dca3933c5b6f2e2305f42187ace9980545435a9
김수혁 <sooo@unist.ac.kr>
2021-08-26 01:38:02 +0900

    ready for packaging

--- loader.py
+++ loader.py
@@ -7,7 +7,7 @@ def load_ingredient(file_path = None):
     Load ingredient data. If file_path is not passed, method loads sample data. It return instance catalog of loaded data.
     """
     if file_path == None:
-        file_path = dirname(__file__) + '/sample_data/sample_ingredients.csv'
+        file_path = dirname(__file__) + '/samples/sample_ingredients.csv'
     raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = 0)
     sample_ingredients = {}
     for idx in raw_df.index:
@@ -25,7 +25,7 @@ def load_menu(ingredients = load_ingredient(), file_path = None):
     Load menu data. If file_path is not passed, method loads sample data. It return instance catalog of loaded data.
     """
     if file_path == None:
-        file_path = dirname(__file__) + '/sample_data/sample_menus.csv'
+        file_path = dirname(__file__) + '/samples/sample_menus.csv'
     raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = None)
     raw_ingredients = pd.DataFrame(data = raw_df['weight'].values, index = pd.MultiIndex.from_frame(raw_df.fillna(method = 'ffill')[['name', 'ingredient']]), columns = ['weight'])
     info = raw_df.loc[pd.notna(raw_df['name']), ['name', 'category', 'note']].set_index('name')
@@ -52,9 +52,9 @@ def load_diet(menus = load_menu(), num_loads = 100, file_path = None, sample_nam
     """
     if file_path == None:
         if sample_name == 'human':
-            file_path = dirname(__file__) + '/sample_data/sample_diet_human.csv'
+            file_path = dirname(__file__) + '/samples/sample_diet_human.csv'
         elif sample_name == 'complete nutrition':
-            file_path = dirname(__file__) + '/sample_data/sample_diet_complete_nutrition.csv'
+            file_path = dirname(__file__) + '/samples/sample_diet_complete_nutrition.csv'
         else:
             raise NameError("For now, the available sample_name is 'human' or 'complete nutrition'")
     raw_df = pd.read_csv(file_path, encoding = 'cp949', index_col = 0).iloc[:num_loads]
